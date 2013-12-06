5a714aba7a50862687e2ae96e925b8579037be52
glencon <glen.conolly@gmail.com>
2013-12-05 20:06:00 -0600

    Started reworking UI according to prototype: http://nathandevuono.ca/mediamanager/

--- Media Manager/MainWindow.xaml.cs
+++ Media Manager/MainWindow.xaml.cs
@@ -20,10 +20,18 @@ namespace Media_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        SolidColorBrush white = new SolidColorBrush(Colors.White);
+        SolidColorBrush black = new SolidColorBrush(Colors.Black);
+        SolidColorBrush grey = new SolidColorBrush(Colors.Gray);
+        Label selected = null;
+        Label[] menu;
+
         public MainWindow()
         {
             InitializeComponent();
-            Movies.ItemsSource = new UserFilesTableAdapters.MoviesTableAdapter().GetData();
+            menu = new Label[2];
+            menu[0] = Movies;
+            menu[1] = TV_Shows;
         }
 
         private void ExitItem_Click(object sender, RoutedEventArgs e)
@@ -36,6 +44,58 @@ namespace Media_Manager
             SettingsWindow settings = new SettingsWindow();
             settings.Owner = this;
             settings.ShowDialog();
+        }
+
+        private void MenuLabel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Label source = (Label)e.Source;
+            paintMouseOver(source);
+        }
+
+        private void MenuLabel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Label source = (Label)e.Source;
+            paintMouseLeave(source);
+        }
+
+        private void MenuLabel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Label source = (Label)e.Source;
+            selected = source;
+            paintSelected(source);
+            redrawOthers();
+        }
+
+        private void redrawOthers()
+        {
+            foreach (Label lbl in menu)
+                paintMouseLeave(lbl);
+        }
+
+        private void paintMouseOver(Label lbl)
+        {
+            lbl.Foreground = white;
+            lbl.Background = grey;
+        }
+
+        private void paintMouseLeave(Label lbl)
+        {
+            if (lbl.Equals(selected))
+                paintSelected(lbl);
+            else
+                paintNormal(lbl);
+        }
+
+        private void paintNormal(Label source)
+        {
+            source.Foreground = black;
+            source.Background = white;
+        }
+
+        private void paintSelected(Label lbl)
+        {
+            lbl.Foreground = white;
+            lbl.Background = black;
         }
     }
 }
